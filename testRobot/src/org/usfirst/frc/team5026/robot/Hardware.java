f127d543601ed51ba8bad7713ed38f9028ad5d8c
Andrew <agcummings11@gmail.com>
2016-01-20 16:26:34 -0800

    Robot, Hardware, and picture updates

--- testRobot/src/org/usfirst/frc/team5026/robot/Hardware.java
+++ testRobot/src/org/usfirst/frc/team5026/robot/Hardware.java
@@ -10,15 +10,15 @@ public class Hardware {
 	
 	// Joysticks
 	public static Joystick primaryDriver = new Joystick(Constants.kPrimaryJoystick);
-	public static Joystick secondaryDriver = new Joystick(Constants.kSecondaryJoystick);
+	//public static Joystick secondaryDriver = new Joystick(Constants.kSecondaryJoystick);
 	
 	//DriveTrain
-	public static ThreeMotorGroup leftDrive = new ThreeMotorGroup(new Talon(Constants.kLeftDrive1), new Talon(Constants.kLeftDrive2), new Talon(Constants.kLeftDrive3), true);
-	public static ThreeMotorGroup rightDrive = new ThreeMotorGroup(new Talon(Constants.kRightDrive1), new Talon(Constants.kRightDrive2), new Talon(Constants.kRightDrive3), false);
+	//public static ThreeMotorGroup leftDrive = new ThreeMotorGroup(new Talon(Constants.kLeftDrive1), new Talon(Constants.kLeftDrive2), new Talon(Constants.kLeftDrive3), true);
+	//public static ThreeMotorGroup rightDrive = new ThreeMotorGroup(new Talon(Constants.kRightDrive1), new Talon(Constants.kRightDrive2), new Talon(Constants.kRightDrive3), false);
 	
 	// DoubleSolenoids
-	public static DoubleSolenoid leftShiftSolenoid = new DoubleSolenoid(Constants.kLeftShiftSolenoidForwardChannel, Constants.kLeftShiftSolenoidReverseChannel);
-	public static DoubleSolenoid rightShiftSolenoid = new DoubleSolenoid(Constants.kRightShiftSolenoidForwardChannel, Constants.kRightShiftSolenoidReverseChannel);
+	//public static DoubleSolenoid leftShiftSolenoid = new DoubleSolenoid(Constants.kLeftShiftSolenoidForwardChannel, Constants.kLeftShiftSolenoidReverseChannel);
+	//public static DoubleSolenoid rightShiftSolenoid = new DoubleSolenoid(Constants.kRightShiftSolenoidForwardChannel, Constants.kRightShiftSolenoidReverseChannel);
 	
 	// Sensors
 	public static Lidar lidar = new Lidar(Constants.kLidarChannel);
--- testRobot/src/org/usfirst/frc/team5026/robot/Robot.java
+++ testRobot/src/org/usfirst/frc/team5026/robot/Robot.java
@@ -94,6 +94,7 @@ public class Robot extends IterativeRobot {
      */
     public void teleopPeriodic() {
         Scheduler.getInstance().run();
+        Hardware.lidar.getDistance();
     }
     
     /**
